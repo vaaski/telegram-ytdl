8722d1487244a6ad596634d00b8c03968bcfad64
vaaski <admin@vaa.ski>
2021-05-06 15:21:16 +0200

    chore(dep): update ytdl-core

--- src/strings.ts
+++ src/strings.ts
@@ -4,17 +4,24 @@ import { bold } from "./util"
 
 const strings = {
   start: (name: string) =>
-    `hello ${name}\n\njust send me a <b>youtube</b> link and i'll give you the choice of downloading it as video or audio.`,
+    `hello ${name}\n\njust send me a ${bold("youtube")} link ` +
+    `and i'll give you the choice of downloading it as video or audio.\n` +
+    `tiktok is also supported, even for videos that have download disabled.`,
+
   startDescription: () => `send the welcome message again`,
+
   unsupported: () => `sorry, i don't know how to download that yet.`,
+
   formatSelection: (video?: string) =>
     `download${video ? " " + bold(video) : ""} as audio or video?`,
+
   downloading: (add?: string) => `downloading${add ? " " + add : ""}...`,
+
   error: (add?: string) => `sorry, something went wrong.` + (add ? "\n\n" + add : ""),
+
   overSize: (type: string, url: string) =>
     `unfortunately, telegram bots can only upload files up to 50MB, and your ${type} is larger than that.\n` +
-    `please visit the link and ` +
-    `${bold("long-press")} or ${bold("right-click")} ` +
+    `please visit the link and ${bold("long-press")} or ${bold("right-click")} ` +
     `and select ${bold("download")} or ${bold("save as")}\n\n` +
     `${url}`,
 }
