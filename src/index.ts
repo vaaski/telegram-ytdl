b5eb2b036bc270c53b2b6c8a7991280e911f5426
Oskar Sommer <oskar.sommer@telekom.de>
2024-01-13 13:19:19 +0100

    grammy

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!")
+import { Bot } from "grammy"
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
+if (!TELEGRAM_BOT_TOKEN) throw new Error("TELEGRAM_BOT_TOKEN is missing")
+
+const bot = new Bot(TELEGRAM_BOT_TOKEN)
+
+bot.on("message", (ctx) => ctx.reply("Hi there!"))
+
+bot.start()
