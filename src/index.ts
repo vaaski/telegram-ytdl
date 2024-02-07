e2dc331375f3b4cbae17203758888e9a52481580
vaaski <admin@vaa.ski>
2024-02-07 13:42:44 +0100

    webhook + local bot api server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import { webhookCallback } from "grammy"
 import { bot } from "./bot"
 import { bold } from "./textutil"
+import express from "express"
+
+const PORT = process.env.TELEGRAM_WEBHOOK_PORT ?? 3000
+if (!PORT) throw new Error("TELEGRAM_WEBHOOK_PORT is missing")
+
+const WEBHOOK_URL = process.env.TELEGRAM_WEBHOOK_URL ?? ""
+if (!WEBHOOK_URL) throw new Error("TELEGRAM_WEBHOOK_URL is missing")
+
+const TELEGRAM_API_ROOT = process.env.TELEGRAM_API_ROOT ?? "http://127.0.0.1:8081"
+if (!TELEGRAM_API_ROOT) throw new Error("TELEGRAM_API_ROOT is missing")
+
+const app = express()
+app.use(express.json())
+
+app.use(webhookCallback(bot, "express"))
 
 bot.use(async (ctx, next) => {
   if (ctx.chat?.type === "private") await next()
@@ -20,5 +36,11 @@ bot.on("message:text", async (ctx) => {
   )
 })
 
-console.log("Starting bot...")
-bot.start()
+console.log(`Starting bot with root ${TELEGRAM_API_ROOT}...`)
+app.listen(PORT, async () => {
+  await bot.api.setWebhook(WEBHOOK_URL)
+  console.log(`Webhook set to ${WEBHOOK_URL}`)
+
+  const me = await bot.api.getMe()
+  console.log(`Bot started as @${me.username} on :${PORT}`)
+})
