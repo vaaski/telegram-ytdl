d3e8ca93b08cf3ddcb5cd810edba70ceeaf51ce1
vaaski <admin@vaa.ski>
2024-08-23 13:47:00 +0200

    Refine denied message for bot; emphasize privacy and provide alternatives without referencing specific content

--- src/constants.ts
+++ src/constants.ts
@@ -1,23 +1,24 @@
 import { bold, link } from "./botutil"
 
 export const deniedMessage = [
-	bold("This bot is now private."),
+	bold("This bot is private."),
 	"",
-	"People have been using it to download Russian propaganda and overall mostly cringy shit and I won't keep paying for servers to support that.",
+	"It costs money to run this and unfortunately it doesn't grow on trees.",
+	`This bot is open source, so you can always ${link("host it yourself", "https://github.com/vaaski/telegram-ytdl#hosting")}.`,
 	"",
 	bold(
 		`As an alternative I recommend checking out ${link(
 			"yt-dlp",
 			"https://github.com/yt-dlp/yt-dlp",
 		)}, the command line tool that powers this bot or ${link(
-			"go-yt-dlp",
-			"https://github.com/vaaski/go-yt-dlp",
-		)}, a simplified wrapper I built for yt-dlp.`,
+			"cobalt",
+			"https://cobalt.tools",
+		)}, a web-based social media content downloader (not affiliated with this bot).`,
 	),
 	"",
 	`${bold(
 		"Do not",
-	)} try to contact me to get whitelisted, I will no longer accept anyone I don't know personally.`,
+	)} try to contact me to get whitelisted, I will not accept anyone I don't know personally.`,
 ].join("\n")
 
 // https://github.com/yt-dlp/yt-dlp/issues/9506#issuecomment-2053987537
